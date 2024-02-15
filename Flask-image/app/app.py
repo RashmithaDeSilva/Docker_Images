6d07df571913a6c97a69c6cea6378211ca9f435e
Lahiru De Silva <lahirurashmitha00@gmail.com>
2024-02-15 16:18:23 +0530

    Add Flask image

---
+++ Flask-image/app/app.py
@@ -0,0 +1,13 @@
+#!/bin/python
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Welcome to Flask !"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
